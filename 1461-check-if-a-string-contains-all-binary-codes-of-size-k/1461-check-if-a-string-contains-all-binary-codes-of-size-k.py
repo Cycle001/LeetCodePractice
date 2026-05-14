2aebf046610820a802be7f9b9335577c91dcc37f
Y.Cheng <157706921+Cycle001@users.noreply.github.com>
2026-05-14 18:54:16 +0800

    05-14-2026 Medium 422 ms (15.79%) 42.9 MB (72.89%) .py Bit Manipulation

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,13 +1,17 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        all_string = set()
-        string = ''
-        for i_r, x in enumerate(s):
-            string += x
-            if i_r < k-1:
-                continue
+        MASK = (1 << k) - 1
+        st = set()  # 更快的写法见另一份代码【Python3 列表】
+        x = 0
+        for i, ch in enumerate(s):
+            # 把 ch 加到 x 的末尾：x 整体左移一位，然后或上 ch
+            # &MASK 目的是去掉超出 k 的比特位
+            x = (x << 1 & MASK) | int(ch)
+            if i >= k - 1:
+                st.add(x)
+        return len(st) == 1 << k
 
-            all_string.add(string)
-            
-            string = string[1:]
-        return len(all_string) == 2**k
+# 作者：灵茶山艾府
+# 链接：https://leetcode.cn/problems/check-if-a-string-contains-all-binary-codes-of-size-k/solutions/3902489/liang-chong-fang-fa-bao-li-wei-yun-suan-4wnph/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
