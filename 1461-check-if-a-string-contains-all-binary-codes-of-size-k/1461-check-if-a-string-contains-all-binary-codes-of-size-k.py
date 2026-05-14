4fc9678712ef737562ccf2511d8ccb3081c8c6d9
Y.Cheng <157706921+Cycle001@users.noreply.github.com>
2026-05-14 16:13:52 +0800

    05-14-2026 Hash Table Medium 299 ms (25.64%) 52.1 MB (17.29%) .py

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+
+        all_string = set()
+        string = ''
+        for i_r, x in enumerate(s):
+            string += x
+            if i_r < k-1:
+                continue
+
+            all_string.add(string)
+            
+            string = string[1:]
+        return len(all_string) == 2**k
